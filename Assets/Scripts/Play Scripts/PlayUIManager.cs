18c7270d5477279e0647e5cc3efe6030482d6f9f
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2021-06-22 17:05:51 -0700

    feat: Implement play scene minus input part

---
+++ Assets/Scripts/Play Scripts/PlayUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayUIManager : MonoBehaviour
+{
+    public static PlayUIManager instance;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Debug.LogError("An instance of PlayUIManager already exists.");
+        }
+
+        audioSource = GameManager.instance.audioSource;
+        audioSource.clip = GameManager.instance.audioClip;
+        audioSource.Play();
+    }
+
+    public void Home() {
+        SceneManager.LoadScene("Home");
+    }
+
+}
